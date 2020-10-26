1ad2e0ad633ede0b2c1744bdd25a7ceba40a7ea9
Jorge Coca <jorge.coca.github@gmail.com>
2020-10-26 08:44:05 -0500

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function shouldCalculateCoverageForFile(fileName, excludedFiles) {
   let isExcluded = false;
   if (excludedFiles.forEach(element => {
     isExcluded = minimatch(fileName, element);
-    return;
+    break;
   }));
   if (isExcluded) {
     core.debug(`Excluding ${fileName} from coverage`);
