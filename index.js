03275e99cea6981679254681dbdb65b52155b88c
Felix Angelov <felangelov@gmail.com>
2020-11-10 16:28:27 -0600

    chore: v1.1.1 (#32)

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const core = require("@actions/core");
-const minimatch = require("minimatch");
-const parse = require("lcov-parse");
+const core = require('@actions/core');
+const minimatch = require('minimatch');
+const parse = require('lcov-parse');
 
 function run() {
-  const lcovPath = core.getInput("path");
-  const minCoverage = core.getInput("min_coverage");
-  const excluded = core.getInput("exclude");
-  const excludedFiles = excluded.split(" ");
+  const lcovPath = core.getInput('path');
+  const minCoverage = core.getInput('min_coverage');
+  const excluded = core.getInput('exclude');
+  const excludedFiles = excluded.split(' ');
 
-  parse(lcovPath, function (_, data) {
-    if (typeof data === "undefined") {
-      core.setFailed("parsing error!");
+  parse(lcovPath, (_, data) => {
+    if (typeof data === 'undefined') {
+      core.setFailed('parsing error!');
       return;
     }
     let totalFinds = 0;
     let totalHits = 0;
-    data.forEach(element => {
-      if (shouldCalculateCoverageForFile(element["file"], excludedFiles)) {
+    data.forEach((element) => {
+      if (shouldCalculateCoverageForFile(element['file'], excludedFiles)) {
         totalHits += element['lines']['hit'];
         totalFinds += element['lines']['found'];
       }
@@ -24,7 +24,7 @@ function run() {
     const coverage = (totalHits / totalFinds) * 100;
     const isValidBuild = coverage >= minCoverage;
     if (!isValidBuild) {
-      core.setFailed(`Coverage ${coverage} is below the minimum ${minCoverage} expected`);
+      core.setFailed(`${coverage} is less than min_coverage ${minCoverage}`);
     }
   });
 }
