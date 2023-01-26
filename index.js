5cd5a92c8da6d81d1d728182dc98250566f85eff
alestiago <dev@alestiago.com>
2023-01-26 12:53:34 +0000

    refactor: decoupling procedures

--- index.js
+++ index.js
@@ -8,88 +8,87 @@ const fs = require('fs');
 ///
 /// We identify comments as "ours" if it was commented by a bot and
 /// the body of the comment contains the signature.
-const commentSignature = `VeryGoodCoverage`;
+const commentSignature = `<!-- VeryGoodCoverage Bot Message -->`;
 
-function run() {
+async function run() {
   const lcovPath = core.getInput('path');
+  if (!canParse(lcovPath)) return;
+
   const minCoverage = core.getInput('min_coverage');
   const excluded = core.getInput('exclude');
   const excludedFiles = excluded.split(' ');
   const githubToken = core.getInput('github_token');
   const reportCoverageComment = core.getInput('report_coverage_comment');
 
-  if (!canParse(lcovPath)) {
-    return;
-  }
-
+  let coverageData;
   parse(lcovPath, (err, data) => {
     if (typeof data === 'undefined') {
       core.setFailed('parsing error!');
-      return;
-    }
-
-    const linesMissingCoverage = [];
-
-    let totalFinds = 0;
-    let totalHits = 0;
-    data.forEach((element) => {
-      if (shouldCalculateCoverageForFile(element['file'], excludedFiles)) {
-        totalFinds += element['lines']['found'];
-        totalHits += element['lines']['hit'];
-
-        for (const lineDetails of element['lines']['details']) {
-          const hits = lineDetails['hit'];
-
-          if (hits === 0) {
-            const fileName = element['file'];
-            const lineNumber = lineDetails['line'];
-            linesMissingCoverage[fileName] =
-              linesMissingCoverage[fileName] || [];
-            linesMissingCoverage[fileName].push(lineNumber);
-          }
-        }
-      }
-    });
-
-    const coverage = (totalHits / totalFinds) * 100;
-    const reachedCoverage = coverage >= minCoverage;
-    const linesMissingCoverageByFile = Object.entries(linesMissingCoverage).map(
-      ([file, lines]) => {
-        return `- ${file}: ${lines.join(', ')}`;
-      }
-    );
-
-    if (!reachedCoverage) {
-      core.setFailed(
-        `${coverage} is less than min_coverage ${minCoverage}\n\n` +
-          'Lines not covered:\n' +
-          linesMissingCoverageByFile.map((line) => `  ${line}`).join('\n')
-      );
-    }
-
-    if (reportCoverageComment && githubToken) {
-      const coverageDifference = coverage - minCoverage;
-      let message = `\
-## ${reachedCoverage ? '✅' : '❌'} ${commentSignature}
-
-Coverage: ${coverage}% (${totalHits} of ${totalFinds} lines)
-Min coverage difference: ${coverageDifference}% (${coverage}% / ${minCoverage}%)
-`;
-
-      if (linesMissingCoverageByFile.length > 0) {
-        message += `\
-<details>
-<summary>Lines not covered</summary>
-${linesMissingCoverageByFile.map((line) => `  ${line}`).join('\n')}
-</details>
-`;
-      }
-
-      postOrUpdateComment(githubToken, message);
+    } else {
+      coverageData = data;
     }
   });
+
+  const linesMissingCoverage = [];
+  let totalFinds = 0;
+  let totalHits = 0;
+  coverageData.forEach((element) => {
+    if (shouldCalculateCoverageForFile(element['file'], excludedFiles)) {
+      totalFinds += element['lines']['found'];
+      totalHits += element['lines']['hit'];
+
+      for (const lineDetails of element['lines']['details']) {
+        const hits = lineDetails['hit'];
+
+        if (hits === 0) {
+          const fileName = element['file'];
+          const lineNumber = lineDetails['line'];
+          linesMissingCoverage[fileName] =
+            linesMissingCoverage[fileName] || [];
+          linesMissingCoverage[fileName].push(lineNumber);
+        }
+      }
+    }
+  });
+
+  const coverage = (totalHits / totalFinds) * 100;
+  const reachedCoverage = coverage >= minCoverage;
+  const linesMissingCoverageByFile = Object.entries(linesMissingCoverage).map(
+    ([file, lines]) => {
+      return `- ${file}: ${lines.join(', ')}`;
+    }
+  );
+
+  if (!reachedCoverage) {
+    core.setFailed(
+      `${coverage} is less than min_coverage ${minCoverage}\n\n` +
+        'Lines not covered:\n' +
+        linesMissingCoverageByFile.map((line) => `  ${line}`).join('\n')
+    );
+  }
+
+  if (reportCoverageComment && githubToken) {
+    const commentIdentifier = await getSignedBotCommentIdentifier();
+    const message = formatCoverageAsMessage({
+      linesMissingCoverageByFile,
+      coverage,
+      minCoverage,
+    });
+
+    if (commentIdentifier) {
+       updateGitHubComment(githubToken, commentIdentifier, message);
+    } else {
+      postGitHubComment(githubToken, message);
+    }
+  }
 }
 
+/**
+ * Whether the given file should be included in the coverage calculation.
+ * 
+ * @param {string} fileName - The name of the file.
+ * @param {string[]} excludedFiles - The list of files to exclude.
+ */
 function shouldCalculateCoverageForFile(fileName, excludedFiles) {
   for (let i = 0; i < excludedFiles.length; i++) {
     const isExcluded = minimatch(fileName, excludedFiles[i]);
@@ -101,40 +100,76 @@ function shouldCalculateCoverageForFile(fileName, excludedFiles) {
   return true;
 }
 
+/**
+ * Whether the lcov file can be parsed or not.
+ * 
+ * @param {string} path - The path to the lcov file.
+ */
 function canParse(path) {
   if (fs.existsSync(path) && fs.readFileSync(path).length === 0) {
     core.setFailed('lcov is empty!');
     return false;
   }
-
   return true;
 }
 
 /**
- * Comments on the GitHub PR with the given message.
- *
- * If a comment already exists, it will be updated; in order to avoid
- * polluting the comment history.
- *
- * To be able to identify "our" comments, the message must
- * contain the `commentSignature`.
- *
- * @param {string} githubToken
- * @param {string} message
- * @returns
+ * Formats the coverage information as a human readable string to be used
+ * as a comment on the GitHub PR.
+ * 
+ * @param {number} coverage
+ * @param {number} minCoverage
+ * 
+ * @returns 
  */
-async function postOrUpdateComment(githubToken, message) {
+function formatCoverageAsMessage({
+  linesMissingCoverageByFile,
+  coverage,
+  totalHits,
+  totalFinds,
+  minCoverage,
+}) {
+  const coverageDifference = coverage - minCoverage;
+  let message = `\
+## ${reachedCoverage ? '✅' : '❌'} ${commentSignature}
+
+Coverage: ${coverage}% (${totalHits} of ${totalFinds} lines)
+Min coverage difference: ${coverageDifference}% (${coverage}% / ${minCoverage}%)
+`;
+
+  if (linesMissingCoverageByFile.length > 0) {
+    message += `\
+<details>
+<summary>Lines not covered</summary>
+${linesMissingCoverageByFile.map((line) => `  ${line}`).join('\n')}
+</details>
+`;
+  }
+
+  return message;
+}
+
+/**
+ * Retrieves the comment identifier of the comment posted by the bot with the
+ * given signature.
+ * 
+ * If no comment was found, `undefined` is returned. If more than one comment was
+ * found, the first one is returned.
+ * 
+ * @param {string} githubToken 
+ * @param {string} signature
+ * @returns {Promise<number?>} The comment identifier, or `undefined` if no comment was found.
+ */
+async function getSignedBotCommentIdentifier(githubToken, signature) {
   if (!githubToken) return;
 
   const octokit = github.getOctokit(githubToken);
   const context = github.context;
-
   const githubIssueData = {
     ...context.repo,
     issue_number: context.issue.number,
   };
 
-  let commentIdentifier;
   const previousComments = await octokit.rest.issues.listComments(
     githubIssueData
   );
@@ -142,23 +177,55 @@ async function postOrUpdateComment(githubToken, message) {
     const comment = previousComments.data[i];
     if (
       comment.user.type === 'Bot' &&
-      comment.body.includes(commentSignature)
+      comment.body.includes(signature)
     ) {
-      commentIdentifier = comment.id;
-      break;
+      return comment.id;
     }
   }
+}
 
+/**
+ * Updates an existing comment on the GitHub PR. 
+ * 
+ * Updating a comment completely replaces the previous comment with the new
+ * message.
+ * 
+ * @param {string} githubToken
+ * @param {number} commentId - The identifier of the comment to update.
+ * @param {string} message - The message to update.
+ */
+async function updateGitHubComment(githubToken, commentId, message) {
+  if (!githubToken) return;
+
+  const octokit = github.getOctokit(githubToken);
+  const context = github.context;
   const comment = {
-    ...githubIssueData,
+    ...context.repo,
+    issue_number: context.issue.number,
+    comment_id: commentId,
     body: message,
-    comment_id: commentIdentifier,
   };
-  if (commentIdentifier) {
-    octokit.rest.issues.updateComment(comment);
-  } else {
-    octokit.rest.issues.createComment(comment);
-  }
+  await octokit.rest.issues.updateComment(comment);
+}
+
+/**
+ * Posts a new comment on the GitHub PR.
+ * 
+ * @param {string} githubToken
+ * @param {string} message - The message to post.
+ * @returns
+ **/
+async function postGitHubComment(githubToken, message) {
+  if (!githubToken) return;
+
+  const octokit = github.getOctokit(githubToken);
+  const context = github.context;
+  const comment = {
+    ...context.repo,
+    issue_number: context.issue.number,
+    body: message,
+  };
+  await octokit.rest.issues.createComment(comment);
 }
 
 run();
